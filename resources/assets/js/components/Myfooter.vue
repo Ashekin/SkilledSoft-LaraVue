29149de9e05ae4b26e0c4b0b9b4981eb9ec88763
Ashekin <mahadi.prof@gmail.com>
2018-07-31 13:01:06 +0600

    VueLara

---
+++ resources/assets/js/components/Myfooter.vue
@@ -0,0 +1,9 @@
+<template>
+<footer class="footer">
+    <div class="content has-text-centered">
+        <p>
+            <strong>Vuejs PhoneBook App</strong> by <a href="https://jgthms.com">Ashekin Mahadi</a>
+        </p>
+    </div>
+</footer>
+</template>
